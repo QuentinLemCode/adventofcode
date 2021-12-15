a9f5f1f6d72c0f2597520442a8109e5c7b6801ac
Quentin Lem <quentinlemcode@gmail.com>
2021-12-15 11:41:10 +0100

    Day9 part2

--- day9/src/index.ts
+++ day9/src/index.ts
@@ -2,18 +2,82 @@ import { readFileSync } from "fs";
 
 const lines = readFileSync('input.txt', 'utf-8').split('\n');
 const coordinates = lines.map(l => l.split('').map(h => Number.parseInt(h)));
+const basins: Basin[] = [];
 
-let score = 0;
-for (const [rowIndex, row] of coordinates.entries()) {
-    for (const [columnIndex, column] of row.entries()) {
-        if (isLower(rowIndex, columnIndex)) {
-            score += column + 1;
+class Point {
+    x: number;
+    y: number;
+    constructor(x: number, y:number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class PointSet<T extends Point> extends Set<T> {
+    add(value: T): this {
+        if(!this.has(value)) {
+            super.add(value);
+        }
+        return this;
+    }
+    has(value: T): boolean {
+        let found = false;
+        this.forEach(item => {
+            if(value.x === item.x && value.y === item.y) {
+                found = true;
+            }
+        });
+        return found;
+    }
+}
+
+class Basin {
+    private set: PointSet<Point>;
+    constructor(lowerPoint: Point) {
+        this.set = new PointSet([lowerPoint]);
+    }
+
+    get size() {
+        return this.set.size;
+    }
+
+    exploreBasin() {
+        let added = true;
+        while(added) {
+            console.log(this.size);
+            added = false;
+            this.set.forEach(point => {
+                for(const [vecX, vecY] of [[0,1], [1,0], [0, -1], [-1, 0]]) {
+                    const X = point.x + vecX;
+                    const Y = point.y + vecY;
+                    if(X < 0 || X >= coordinates.length || Y < 0 || Y >= coordinates[X].length) {
+                        continue;
+                    }
+                    if (coordinates[X][Y] !== 9) {
+                        const point = new Point(X,Y);
+                        added = !this.set.has(point);
+                        this.set.add(point);
+                    }
+                }
+            });
         }
     }
 }
 
-console.log(score);
 
+for (const [rowIndex, row] of coordinates.entries()) {
+    for (const [columnIndex, column] of row.entries()) {
+        if (isLower(rowIndex, columnIndex)) {
+            basins.push(new Basin(new Point(rowIndex, columnIndex)));
+        }
+    }
+}
+
+basins.forEach(basin => basin.exploreBasin());
+
+basins.sort((a, b) => b.size - a.size);
+
+console.log(basins[0].size * basins[1].size * basins[2].size);
 
 function isLower(x: number, y: number) {
     const depth = coordinates[x][y];
