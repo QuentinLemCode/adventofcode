a233f4f3333e35dbd66b10772c3bc45088ca0550
Quentin Lem <quentinlemcode@gmail.com>
2021-12-15 12:04:01 +0100

    Day10 part 1

---
+++ day10/src/index.ts
@@ -0,0 +1,39 @@
+import { ReadFile } from './fileread';
+
+const file = new ReadFile('input.txt');
+const stack: string[] = [];
+const opening = ['(', '[', '{', '<'];
+const closing = [')', ']', '}', '>'];
+const scores = [3, 57, 1197, 25137];
+let score = 0;
+
+function getClosing(char: string) {
+  const idx = opening.findIndex(a => a === char);
+  return closing[idx];
+}
+
+function isOpening(char: string) {
+  return opening.find(a => a === char);
+}
+
+function onEachLine(line: string) {
+  const chars = line.split('');
+  for (const char of chars) {
+    if (isOpening(char)) {
+      stack.push(char);
+    } else {
+      const closeChar = getClosing(stack.pop() || '');
+      if (char !== closeChar) {
+        console.log('expected ' + closeChar + ' received ' + char);
+        score += scores[closing.findIndex(a => a === char)];
+        return;
+      }
+    }
+  }
+}
+
+async function process() {
+  await file.applyFunction(onEachLine);
+  console.log(score);
+}
+process().then();
