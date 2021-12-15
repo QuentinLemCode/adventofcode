24e483925a75152195519ff927de034aea1cfa6f
Quentin Lem <quentinlemcode@gmail.com>
2021-12-15 12:25:12 +0100

    Day10 part 2

--- day10/src/index-part1.ts
+++ day10/src/index-part1.ts
@@ -3,8 +3,8 @@ import { ReadFile } from './fileread';
 const file = new ReadFile('input.txt');
 const opening = ['(', '[', '{', '<'];
 const closing = [')', ']', '}', '>'];
-const scores = [1, 2, 3, 4];
-const allScores: number[] = [];
+const scores = [3, 57, 1197, 25137];
+let score = 0;
 
 function getClosing(char: string) {
   const idx = opening.findIndex(a => a === char);
@@ -18,31 +18,22 @@ function isOpening(char: string) {
 function onEachLine(line: string) {
   const stack: string[] = [];
   const chars = line.split('');
-  let corrupted = false;
   for (const char of chars) {
     if (isOpening(char)) {
       stack.push(char);
     } else {
       const closeChar = getClosing(stack.pop() || '');
       if (char !== closeChar) {
-        corrupted = true;
+        console.log('expected ' + closeChar + ' received ' + char);
+        score += scores[closing.findIndex(a => a === char)];
         return;
       }
     }
-  }
-
-  if (!corrupted && stack.length > 0) {
-    let lineScore = 0;
-    for (let char = stack.pop(); char !== undefined; char = stack.pop()) {
-      lineScore *= 5;
-      lineScore += scores[opening.findIndex(a => a === char)];
-    }
-    allScores.push(lineScore);
   }
 }
 
 async function process() {
   await file.applyFunction(onEachLine);
-  console.log(allScores.sort()[Math.floor(allScores.length / 2)]);
+  console.log(score);
 }
 process().then();
